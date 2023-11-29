c374bb86dd4a81299a3d2b0d524d021292f0955b
RahulGorai0206 <rahulgorai0206@gmail.com>
2023-11-29 17:05:03 +0530

    fix 404 issue

--- js/home.js
+++ js/home.js
@@ -150,31 +150,3 @@ fetchAndDisplayNotifications();
 
 // Set interval to fetch and display notifications every 2 seconds
 setInterval(fetchAndDisplayNotifications, 2000);
-
-
-function getCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-        if (c.indexOf(nameEQ) === 0) return c.substring(nameEQ.length, c.length);
-    }
-    return null;
-}
-
-function deleteCookie(name) {
-    document.cookie = name + '=; Max-Age=-99999999;';
-}
-
-function logout() {
-    deleteCookie("authToken");
-    window.location.href = "../index.html";
-}
-
-window.onload = function () {
-    var authToken = getCookie("authToken");
-    if (!authToken) {
-        window.location.href = "../index.html";
-    }
-};
